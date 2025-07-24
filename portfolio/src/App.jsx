65cce60d14d5cd6cebe9df266e94e86661fc6268
aliyu aminu adavatu <aminu-ad_a@qwasar.io>
2025-07-24 15:52:43 +0100

    Added a Darkmode theme for global pages

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Contact from './components/Contact';
 import Header from './components/Header';
@@ -8,18 +8,22 @@ import Portfolio from './components/Portfolio';
 import Home from './components/Home';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<About />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/portfolio' element={<Portfolio />} />
-      </Routes>
-    </BrowserRouter>
-  )
+   <div className={`${darkMode ? 'bg-black text-white' : 'bg-white text-black'} min-h-screen w-full overflow-x-hidden`}>
+      <BrowserRouter>
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
